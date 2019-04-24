5974e7860cf9edf27ef88c130666ca002434dc42
Rishabh Singhal <rishabh.singhal@cygrp.com>
2019-04-24 23:50:41 +0530

    done

--- src/Disease.js
+++ src/Disease.js
@@ -4,90 +4,95 @@ import DiseaseAsk from "./DiseaseAsk";
 
 class Disease extends React.Component {
   state = {
-    symptom: "fever",
-    sym: "fever",
-    value: "",
-    dispaly: "block",
-    dispalyy: "none"
+    disease: {
+      symptom: "fever",
+      display: "block",
+      displayy: "none"
+    },
+    value: ""
   };
+
   yes = {
     fever: "headache",
-    neusia: "hepatitis(2.0/1.0)",
-    headache: "redcoloured_rash",
-    redcoloured_rash: "Chicken_Pox"
+    headache: "Vometting",
+    Vometting: "Joints_and_muscle_pain",
+    weakness: "Weight_loss",
+    headache2: "chest_pain",
+    Weight_loss: "Frequent_Urination",
+    Frequent_Urination: "Diabetes",
+    chest_pain: "Blood_Pressure",
+    Neck_Pain: "servical",
+    Itches_and_rashes: "chicken_pox",
+    Joints_and_muscle_pain: "Dengue",
+    Cough: "swine_flue",
+    Abdominal_pain: "Malaria"
   };
   no = {
-    fever: "neusia",
-    neusia: "measies(2.0/1.0)",
-    headache: "Poliomylitis(3.0/2.0)",
-    redcoloured_rash: "Rabies(3.0/2.0)"
+    fever: "weakness",
+    headache: "viral_fever",
+    Vometting: "Itches_and_rashes",
+    weakness: "headache2",
+    headache2: "CAN'T",
+    Weight_loss: "CAN'T",
+    Frequent_Urination: "Thyroid",
+    chest_pain: "Neck_Pain",
+    Neck_Pain: "Higrains",
+    Itches_and_rashes: "Typhoid",
+    Joints_and_muscle_pain: "Cough",
+    Cough: "Abdominal_pain",
+    Abdominal_pain: "CAN'T"
   };
   Disease_List = [
-    "hepatitis(2.0/1.0)",
-    "Chicken_Pox",
-    "measies(2.0/1.0)",
-    "Poliomylitis(3.0/2.0)",
-    "Rabies(3.0/2.0)"
+    "viral_fever",
+    "CAN'T",
+    "Blood_Pressure",
+    "Higrains",
+    "servical",
+    "Typhoid",
+    "Dengue",
+    "swine_flue",
+    "Thyroid",
+    "Diabetes"
   ];
-  handledisease = () => {
-    var sym = this.state.symptom;
-    console.log(this.Disease_List.includes(sym));
 
+  handledisease = () => {
     if (this.state.value === "Yes") {
-      if (sym === "fever") {
-        this.setState({
-          symptom: this.yes.fever
-        });
-      }
-      if (sym === "headache") {
-        this.setState({
-          symptom: this.yes.headache
-        });
-      }
-      if (sym === "neusia") {
-        this.setState({
-          symptom: this.yes.neusia,
-          dispaly: "none",
-          dispalyy: "block"
-        });
-      }
-      if (sym === "redcoloured_rash") {
-        this.setState({
-          symptom: this.yes.redcoloured_rash,
-          dispaly: "none",
-          dispalyy: "block"
-        });
-      }
-    }
-    if (this.state.value === "No") {
-      if (sym === "fever") {
-        this.setState({
-          symptom: this.no.fever
-        });
-      }
-      if (sym === "headache") {
-        this.setState({
-          symptom: this.no.headache,
-          dispaly: "none",
-          dispalyy: "block"
-        });
-      }
-      if (sym === "neusia") {
-        this.setState({
-          symptom: this.no.neusia,
-          dispaly: "none",
-          dispalyy: "block"
-        });
-      }
-      if (sym === "redcoloured_rash") {
-        this.setState({
-          symptom: this.no.redcoloured_rash,
-          dispaly: "none",
-          dispalyy: "block"
-        });
-      }
+      this.setState(prevState => {
+        let updatedUserInfo = prevState.disease;
+        updatedUserInfo["symptom"] = this.yes[this.state.disease.symptom];
+        if (this.Disease_List.includes(this.state.disease.symptom)) {
+          updatedUserInfo["display"] = "none";
+          updatedUserInfo["displayy"] = "block";
+        } else {
+          updatedUserInfo["display"] = "block";
+          updatedUserInfo["displayy"] = "none";
+        }
+        return {
+          disease: {
+            ...updatedUserInfo
+          }
+        };
+      });
+    } else {
+      this.setState(prevState => {
+        let updatedUserInfo = prevState.disease;
+        updatedUserInfo["symptom"] = this.no[this.state.disease.symptom];
+        if (this.Disease_List.includes(this.state.disease.symptom)) {
+          updatedUserInfo["display"] = "none";
+          updatedUserInfo["displayy"] = "block";
+        } else {
+          updatedUserInfo["display"] = "block";
+          updatedUserInfo["displayy"] = "none";
+        }
+        return {
+          disease: {
+            ...updatedUserInfo
+          }
+        };
+      });
     }
   };
+
   handlechange = event => {
     const { name, value } = event.target;
     if (name === "choice") {
@@ -99,9 +104,14 @@ class Disease extends React.Component {
   render() {
     return (
       <div className="classmain">
-        <div><div className="disease">Disease Pridiction</div></div>
-        <div style={{ display: this.state.dispaly }} className="classask">
-          <DiseaseAsk symptoms={this.state.symptom} />
+        <div>
+          <div className="disease">Disease Prediction</div>
+        </div>
+        <div
+          style={{ display: this.state.disease.display }}
+          className="classask"
+        >
+          <DiseaseAsk symptoms={this.state.disease.symptom} />
           <input
             type="radio"
             value="Yes"
@@ -120,12 +130,29 @@ class Disease extends React.Component {
           />{" "}
           No
           <hr />
-          <input type="button" value="Next" onClick={this.handledisease} className="button"/>
+          <input
+            type="button"
+            value="Next"
+            onClick={this.handledisease}
+            className="button"
+          />
         </div>
-        <div style={{ display: this.state.dispalyy }} className="classresult">
-          <h3>You have <span style={{color: "red"}}>{this.state.symptom}</span></h3>
-          <h3>Priscribed Doctor <span style={{color: "green"}}>{this.state.symptom}</span></h3>
-          <h3>Priscribed Medicine <span style={{color: "green"}}>{this.state.symptom}</span></h3>
+        <div
+          style={{ display: this.state.disease.displayy }}
+          className="classresult"
+        >
+          <h3>
+            You have{" "}
+            <span style={{ color: "red" }}>{this.state.disease.symptom}</span>
+          </h3>
+          <h3>
+            Priscribed Doctor{" "}
+            <span style={{ color: "green" }}>Dr. Maheswari</span>
+          </h3>
+          <h3>
+            Priscribed Medicine{" "}
+            <span style={{ color: "green" }}>Vobit 0.3</span>
+          </h3>
         </div>
       </div>
     );
--- src/DiseaseAsk.js
+++ src/DiseaseAsk.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-class DiseaseAsk extends React.Component{
-    render(){
-        return(
-            <div className="component">
-            <h3>Do tou have : <span style={{color: "red"}}>{this.props.symptoms} </span>
-            </h3>
-            
-            </div>
-        );
-    }
-} 
+class DiseaseAsk extends React.Component {
+  render() {
+    return (
+      <div className="component">
+        <h3>
+          Do You have :{" "}
+          <span style={{ color: "red" }}>{this.props.symptoms} </span>
+        </h3>
+      </div>
+    );
+  }
+}
 
 export default DiseaseAsk;
